fbae6d1dff32147dc8e163890c031e658dc83f41
looopas <skirmabrabus@gmail.com>
2018-01-13 16:44:13 +0200

    visos crypto nuskaitomos is crypto marcket

--- bandymeliai.py
+++ bandymeliai.py
@@ -5,7 +5,7 @@ from models.positionmodel import BookModel
 
 
 def update_current():
-    content=requests.get('https://api.coinmarketcap.com/v1/ticker/')
+    content=requests.get('https://api.coinmarketcap.com/v1/ticker/?limit=0')
     content=content.json()
     for i in content:
         position = BookModel.find_by_name(i['symbol'].lower())

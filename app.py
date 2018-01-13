5273355a9854b9549e5500e6b646133014983d4b
looopas <skirmabrabus@gmail.com>
2018-01-13 18:54:49 +0200

    removed URL sqlite

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 

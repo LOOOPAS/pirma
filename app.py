85ee961c159f87771fc80daad5e54784685afba8
looopas <skirmabrabus@gmail.com>
2018-01-13 16:28:42 +0200

    fixed db not defined

--- app.py
+++ app.py
@@ -140,9 +140,7 @@ def pasirinkimas():
 
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+
 
 
 
@@ -153,6 +151,4 @@ def create_tables():
 # api.add_resource(Finished, '/finished')
 # api.add_resource(Allusd, '/allusd')
 if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
     app.run(port=5000, debug=True)
